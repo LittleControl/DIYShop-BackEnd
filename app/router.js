4a21be5b36e2ced2f5e7713ad42b9eae7288c1a8
LittleControl <i@littlecontrol.cc>
2020-04-22 16:16:14 +0800

    根据IP显示地址

---
+++ app/router.js
@@ -0,0 +1,7 @@
+module.exports = app => {
+    const { router, controller } = app;
+    router.get('/', controller.home.index);
+    router.get('/user', controller.user.echo)
+    router.get('/user/:name', controller.user.info);
+    router.get('/ip', controller.user.ip)
+}
---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+exports.keys = '<littlecontrol@qq.com>'
+exports.mongoose = {
+    client: {
+        url: 'mongodb://127.0.0.1/diyshop',
+        options: {},
+        plugins: []
+        // mongoose global plugins, expected a function or an array of function and options
+        // plugins: [createdPlugin, [updatedPlugin, pluginOptions]],
+    },
+};
