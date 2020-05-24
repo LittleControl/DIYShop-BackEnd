3e99e8b7df281ff59e191f6376aaa5d7a26db20f
LittleControl <littlecontrol2019@gmail.com>
2020-05-25 07:43:24 +0800

    不小心更改了数据库名称

--- config/config.default.js
+++ config/config.default.js
@@ -1,7 +1,7 @@
 exports.keys = '<littlecontrol@qq.com>'
 exports.mongoose = {
     client: {
-        url: 'mongodb://127.0.0.1/diyshop',
+        url: 'mongodb://127.0.0.1/diy_shop',
         options: {},
         plugins: []
         // mongoose global plugins, expected a function or an array of function and options
