8fdeb7d0675dad075da838e8191030d4e4cf984c
LittleControl <littlecontrol2019@gmail.com>
2020-06-17 10:32:50 +0800

    优化部分写法

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,10 +2,12 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
     async index() {
-        this.ctx.body = await this.service.home.index()
+        const { ctx } = this
+        ctx.body = 'Hello, home index'
     }
     async banners() {
-        this.ctx.body = await this.service.home.getBanners()
+        const { ctx, service } = this
+        ctx.body = await service.home.getBanners()
     }
 }
 
--- app/service/home.js
+++ app/service/home.js
@@ -3,9 +3,6 @@
 const Service = require('egg').Service;
 
 class HomeService extends Service {
-    index() {
-        return 'Hello, Home'
-    }
     async getBanners() {
         let banners = this.app.bannerModel
         let res = await banners.find()
