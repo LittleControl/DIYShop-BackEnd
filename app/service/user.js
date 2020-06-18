cc24e2aeeaa9dc6e65d55a5e39829f4013e2af42
LittleControl <littlecontrol2019@gmail.com>
2020-06-18 23:40:54 +0800

    尝试注册

--- app/service/user.js
+++ app/service/user.js
@@ -3,7 +3,7 @@
 const Service = require('egg').Service
 
 class UserService extends Service {
-    async userIP() {
+    async ip() {
         const API = 'https://apis.map.qq.com/ws/location/v1/ip'//腾讯地图API接口
         const KEY = 'LX6BZ-LG733-GHH37-3ZXBJ-HCMUK-FEBMA'//开发者密钥
         let res = await this.ctx.curl(API, {
@@ -14,14 +14,25 @@ class UserService extends Service {
         })
         return JSON.parse(res.data).result
     }
-    async userInfo(email, password) {
-        let user = this.app.userModel
+    async info(email, password) {
+        const user = this.app.userModel
         let res = await user.findOne({ email, password }, (err, data) => {
             if (err) {
-                console.log('Database Error!')
                 return { msg: 'Database Error!' }
             }
             return data
+        })
+        return res
+    }
+    async signup(email, password, name, bio) {
+        const user = this.app.userModel
+        let res = await user.findOneAndUpdate({ email, password, name, bio }, { upsert: true }, (err, data) => {
+            if (err) {
+                return {
+                    msg: 'Database Error!'
+                }
+            }
+            return 'OKay!'
         })
         return res
     }
