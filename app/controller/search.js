ecc197bd595af7bf5c11a6d757db5d59671883f4
LittleControl <littlecontrol2019@gmail.com>
2020-07-11 15:38:27 +0800

    搜索商家功能

---
+++ app/controller/search.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class SearchController extends Controller {
+    async shops() {
+        const { ctx, service } = this
+        const { keyword } = ctx.request.query
+        console.log(keyword)
+        const shops = await service.search.shops(keyword)
+        ctx.body = shops
+    }
+}
+
+module.exports = SearchController;
